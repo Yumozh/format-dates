57544c9e67fc1f8cda74cbb4b9a827855f212242
Yumozh <juliiamozhchil@gmail.com>
2026-04-22 11:22:50 -0500

    Print gmt and local zone time

--- src/main/java/com/pluralsight/Program.java
+++ src/main/java/com/pluralsight/Program.java
@@ -1,17 +1,35 @@
 package com.pluralsight;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class Program {
     public static void main(String[] args) {
-        LocalDate myDate = LocalDate.now();
+//        LocalDateTime myDate = LocalDateTime.now();
+        ZonedDateTime gmtTime = ZonedDateTime.now(ZoneId.of("GMT"));
+        LocalDateTime myDate = LocalDateTime.now();
 
+        DateTimeFormatter gmt = DateTimeFormatter.ofPattern("yyyy-MM-dd");
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+        DateTimeFormatter formatterWeekDay = DateTimeFormatter.ofPattern("EEEE dd, yyyy");
+        DateTimeFormatter formatterWhole = DateTimeFormatter.ofPattern("EEEE, MMM dd, yyyy HH:mm");
+        DateTimeFormatter formatterLocal = DateTimeFormatter.ofPattern("HH:mm 'on' dd-MMM-yyyy");
 
-        String formatterDate = myDate.format(formatter);
+        String formatterGMT = gmtTime.format(gmt);
+        String formatterDate = gmtTime.format(formatter);
+        String formatterDateWeekName = gmtTime.format(formatterWeekDay);
+        String formatterDateWhole = gmtTime.format(formatterWhole);
+        String formatterDataLocal = myDate.format(formatterLocal);
 
         System.out.println(formatterDate);
-        System.out.println(myDate);
+        System.out.println(formatterGMT);
+        System.out.println(formatterDateWeekName);
+        System.out.println(formatterDateWhole);
+        System.out.println();
+        System.out.println(formatterDataLocal);
+
     }
 }
