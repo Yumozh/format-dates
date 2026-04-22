1ea657719ce19c76fcc9627edaf3da68fea996fc
Yumozh <juliiamozhchil@gmail.com>
2026-04-22 10:37:02 -0500

    Initiate local date display and format

--- src/main/java/com/pluralsight/Program.java
+++ src/main/java/com/pluralsight/Program.java
@@ -1,4 +1,17 @@
 package com.pluralsight;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Program {
+    public static void main(String[] args) {
+        LocalDate myDate = LocalDate.now();
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+
+        String formatterDate = myDate.format(formatter);
+
+        System.out.println(formatterDate);
+        System.out.println(myDate);
+    }
 }
